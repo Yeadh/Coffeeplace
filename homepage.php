0ca6e647cf8ed0242ee9f5f68ce9ffda75909ad3
MD. YEADH HASAN BAPPY <yeadhapps@gmail.com>
2017-06-03 11:35:39 +0600

    responsive update

--- homepage.php
+++ homepage.php
@@ -10,7 +10,7 @@ get_header(); ?>
 		<div class="row">
 			<div class="col-md-12">
 				 <div class="pswr out" style="" data-tve-custom-colour="58530977">
-					<div class="header_subscribe_area" data-width="1840" data-height="382" style="background-image: url(&quot;https://www.littlecoffeeplace.com/wp-content/uploads/2016/01/Coffee-black.jpg&quot;);">
+					<div class="header_subscribe_area" data-width="1840" data-height="382" style="background-image: url(http://localhost/coffee/wp-content/uploads/2017/06/Untitled-1-1.png);">
 						<div class="cck tve_clearfix tve_empty_dropzone">
 							<h1 class="tve_p_center gr_ver_2 rft bold_text">
 								<span class="tve_custom_font_size rft">
@@ -73,30 +73,6 @@ get_header(); ?>
 
 				endif; ?>
 			
-			<div class="col-md-6 home_post">
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header home_post_header">
-						<h2 class="entry-title">
-						<a href="#">History of induction cooking?</a>
-						</h2>	
-					</header>
-					<div class="home_images">
-						<a href="#"><img src="http://localhost/coffee/wp-content/uploads/2017/05/Eating-Coffee-Beans.jpg" alt="">
-					</div>
-				</article><!-- #post-## -->
-			</div>
-			<div class="col-md-6 home_post">
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header home_post_header">
-						<h2 class="entry-title">
-						<a href="#">History of induction cooking?</a>
-						</h2>	
-					</header>
-					<div class="home_images">
-						<a href="#"><img src="http://localhost/coffee/wp-content/uploads/2017/05/Eating-Coffee-Beans.jpg" alt="">
-					</div>
-				</article><!-- #post-## -->
-			</div>
 			</div>
 		</div><!-- #row -->
 	</div><!-- #primary -->
